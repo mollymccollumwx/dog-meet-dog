4ac5b4adafeb6b103a0edf2a322445893be407b1
Mario Reid <mario.reid83@gmail.com>
2021-01-29 17:03:59 -0500

    pseudo coded dogController

---
+++ controllers/dogController.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const db = require("../models");
+
+const router = express.Router();
+
+//VIEWS ROUTES
+//GET Route to render index page
+
+
+//GET Route to render signup page
+
+
+//GET Route to dashboard page
+
+
+//GET Route to connections page
+
+//GET Route to edit user page
+
+//GET Route for delete page
+
+//GET Route for treat points page
+
+
+//API ROUTES
+//POST Route create a new user in database
+
+
+//PUT Route to update the user in database
+
+
+//DELETE Route to delete user from database
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
 -- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS project2starter;
+DROP DATABASE IF EXISTS dog_meet_dog;
 -- Creates the "todolist" database --
-CREATE DATABASE project2starter;
+CREATE DATABASE dog_meet_dog;
