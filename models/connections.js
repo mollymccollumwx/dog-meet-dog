85e58578074a81d63689b8a651387ee9f02c82e2
Molly <mollymccollumwx@gmail.com>
2021-01-29 20:42:58 -0500

    added associations to connection and user models

--- models/connections.js
+++ models/connections.js
@@ -1,4 +1,14 @@
 module.exports = function (sequelize, DataTypes) {
   const Connection = sequelize.define("Connection", {});
+  Connection.associate = function (models) {
+    Connection.belongsTo(models.User, {
+      as: "userOne",
+      onDelete: "CASCADE",
+    });
+    Connection.belongsTo(models.User, {
+      as: "userTwo",
+      onDelete: "CASCADE",
+    });
+  };
   return Connection;
 };
--- models/user.js
+++ models/user.js
@@ -12,6 +12,11 @@ module.exports = function (sequelize, DataTypes) {
         imageLink: DataTypes.STRING,
         dogVaccinated: DataTypes.BOOLEAN,
         treatPoints: DataTypes.INTEGER
-    })
+    });
+    User.associate = function(models) {
+       User.belongsToMany(models.User, {through: models.Connection, as: 'userOnes', foreignKey: 'userOneId'});
+       User.belongsToMany(models.User, {through: models.Connection, as: 'userTwos', foreignKey: 'userTwoId'});
+    }
     return User;
+
 };
