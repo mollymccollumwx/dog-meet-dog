0f8b79255151e5f2f0fa9475b14954ba27d5b8e7
Molly <mollymccollumwx@gmail.com>
2021-01-30 20:09:38 -0500

    created seed.sql file and added seed data from mySQL workbench for the users

---
+++ db/seed.sql
@@ -0,0 +1,31 @@
+INSERT INTO Users (ownerFirstName, ownerLastName, email, password, dogName, city, dogSize, friendliness, dogAge, dogBreed, imageLink, dogVaccinated)
+VALUES ("Carrie", "Oakey", "carrieoakey@test.com", "ilovepups", "Snoop", "Atlanta", "Small", "All dog friendly", "Puppy", "Beagle", "http://res.cloudinary.com/deckz8crp/image/upload/v1612052897/fwutxpwlpq9osbehpqa3.jpg", true);
+
+INSERT INTO Users (ownerFirstName, ownerLastName, email, password, dogName, city, dogSize, friendliness, dogAge, dogBreed, imageLink, dogVaccinated, treatPoints)
+VALUES ("Gene", "Poole", "genepoole@test.com", "dogsrule", "Meatball", "Atlanta", "Small", "All dog friendly", "Young", "French Bulldog", "http://res.cloudinary.com/deckz8crp/image/upload/v1612053743/u8vttym2gmkx7k2lsdf3.jpg", true, 70);
+
+INSERT INTO Users (ownerFirstName, ownerLastName, email, password, dogName, city, dogSize, friendliness, dogAge, dogBreed, imageLink, dogVaccinated)
+VALUES ("Joy", "Kill", "joykill@test.com", "kitty123", "Kitty", "Roswell", "Small", "Small dog friendly", "Adult", "Maltese", "http://res.cloudinary.com/deckz8crp/image/upload/v1612054175/sfmniuc7cbq5a9huqzrq.jpg", false);
+
+INSERT INTO Users (ownerFirstName, ownerLastName, email, password, dogName, city, dogSize, friendliness, dogAge, dogBreed, imageLink, dogVaccinated, treatPoints)
+VALUES ("Tim", "Burr", "timburr@test.com", "starwars", "Chewbarka", "Marietta", "Large", "Large dog friendly", "Adult", "German Shepherd", "http://res.cloudinary.com/deckz8crp/image/upload/v1612054514/d8zw21opyhuttdptac5a.jpg", true, 50);
+
+INSERT INTO Users (ownerFirstName, ownerLastName, email, password, dogName, city, dogSize, friendliness, dogAge, dogBreed, imageLink, dogVaccinated, treatPoints)
+VALUES ("Pepe", "Roni", "peperoni@test.com", "mansbestfriend", "Slug", "Sandy Springs", "Extra large", "Large dog friendly", "Senior", "Saint Bernard", "http://res.cloudinary.com/deckz8crp/image/upload/v1612054916/e3vwqtytnpcp4cv1g4cm.jpg", true, 50);
+
+INSERT INTO Users (ownerFirstName, ownerLastName,email,password,
+					dogName,city,dogSize,friendliness,
+                    dogAge,dogBreed,imageLink,dogVaccinated, treatPoints)
+VALUES 
+	("Hannah","Smith", "hannahsmith@test.com", "archie123", 
+    "Archie", "Atlanta", "Large","All dog friendly", 
+    "Young","Golden Retriever", "http://res.cloudinary.com/deckz8crp/image/upload/v1612053546/lpd4zlozg4yvsl5klgzu.jpg", true, 10),
+    ("Mack","Williams","mackwilliams@test.com","porky2348",
+    "Porky","Buckhead","Small", "Small dog friendly",
+    "Adult", "Corgi", "http://res.cloudinary.com/deckz8crp/image/upload/v1612054139/sfzmlldujsvl0tn33au9.jpg", true, 100),
+    ("Willy", "Wonka", "willywonka@test.com" , "scouty47290",
+    "Scout", "Lawrencevile", "Medium" ,"All dog friendly",
+    "Young", "Mix breed" ,"http://res.cloudinary.com/deckz8crp/image/upload/v1612054596/afuiua4nd8iatp0nrxhu.jpg" ,false, 40),
+    ("Polly","Pocket","pollypocket@test.com","pippa12345",
+    "Pippa","Buford","Extra small", "Small dog friendly",
+    "Puppy", "Daschund", "http://res.cloudinary.com/deckz8crp/image/upload/v1612054959/kjveeeqkkwbx53gvgi1w.jpg", true, 200);
