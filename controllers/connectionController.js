897bc87e5ed6d01c682394b63f1e12033ec339be
Molly <mollymccollumwx@gmail.com>
2021-01-31 15:56:10 -0500

    package.json is back from the dead

--- controllers/connectionController.js
+++ controllers/connectionController.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { restart } = require("nodemon");
+
 const db = require("../models");
 
 const router = express.Router();
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { restart } = require("nodemon");
 const db = require("../models");
 
 const router = express.Router();
