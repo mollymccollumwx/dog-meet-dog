25dc238bda49015a4abc10771a10c27b35326432
Jeana Rose <jr.mathis608@gmail.com>
2021-02-01 15:13:50 -0500

    Abstracted API routes out of the server

--- server.js
+++ server.js
@@ -9,20 +9,6 @@ const db = require("./models");
 const UsersController = require("./controllers/userController");
 const ConnectionsController = require("./controllers/connectionController");
 
-const fileupload = require("express-fileupload");
-app.use(
-  fileupload({
-    useTempFiles: true,
-  })
-);
-
-const cloudinary = require("cloudinary").v2;
-cloudinary.config({
-  cloud_name: "deckz8crp",
-  api_key: "425322847743815",
-  api_secret: "ryfL4bUqAh7PbwfeEzxzcmuS0r4",
-});
-
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
@@ -38,33 +24,10 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 
-
-////////CLOUDINARY TRIAL ROUTE///////////////////////////////////
-app.post("/upload/:id", function (req, res, next) {
-  const file = req.files.photo;
-  cloudinary.uploader.upload(file.tempFilePath, function (err, result) {
-    console.log("Error: ", err);
-    console.log("Cloudinary URL: ", result.url);
-    db.User.update(
-      { imageLink: result.url },
-      { where: { id: req.params.id } }
-    ).then((updatedUser) => {
-      console.log(updatedUser);
-      res.json({ success: true });
-    });
-  });
-});
-
-//////////API ROUTES////////////////////////////////
-app.post("/api/signup", function (req, res) {
-  db.User.create(req.body).then(function (dbUser) {
-    res.json(dbUser);
-  });
-});
-
 app.use(UsersController);
 app.use(ConnectionsController);
 require("./routes/html-routes")(app);
+require("./routes/api-routes")(app);
 
 db.sequelize
   .sync()
