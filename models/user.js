ac63186cf51b63f430fcae3ec244b9f8a9f5399f
Mario Reid <mario.reid83@gmail.com>
2021-02-02 18:20:55 -0500

    created alerts for signup form when field is empty

--- models/user.js
+++ models/user.js
@@ -1,26 +1,58 @@
 module.exports = function (sequelize, DataTypes) {
     const User = sequelize.define("User",{
-        ownerFirstName: DataTypes.STRING,
-        ownerLastName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING,
-        dogName: DataTypes.STRING,
-        city: DataTypes.STRING,
-        dogSize: DataTypes.STRING,
-        friendliness: DataTypes.STRING,
-        dogAge: DataTypes.STRING,
-        dogBreed: DataTypes.STRING,
+        ownerFirstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        ownerLastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dogName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dogSize: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        friendliness: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dogAge: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        dogBreed: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         imageLink: {
             type: DataTypes.STRING,
             defaultValue: "http://res.cloudinary.com/deckz8crp/image/upload/v1612142957/hfutnx74mbuoxqndfrwp.png"
         }, 
         dogVaccinated: DataTypes.BOOLEAN,
-        treatPoints: DataTypes.INTEGER
+        treatPoints: {
+            type: DataTypes.INTEGER,
+            defaultValue: 10
+        }
     });
     User.associate = function(models) {
        User.belongsToMany(models.User, {through: models.Connection, as: 'userOnes', foreignKey: 'userOneId'});
        User.belongsToMany(models.User, {through: models.Connection, as: 'userTwos', foreignKey: 'userTwoId'});
     }
     return User;
-
 };
