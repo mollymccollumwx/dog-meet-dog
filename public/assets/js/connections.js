540cd535e6efdf60a5f87c41eb2bbc9abf697195
Jeana Rose <jr.mathis608@gmail.com>
2021-02-02 12:50:35 -0500

    Uncommented modal code in connections.js

--- public/assets/js/connections.js
+++ public/assets/js/connections.js
@@ -12,25 +12,24 @@ $(document).ready(function(){
         window.open("/connections/" + currentUser,"_self")
       })
     })
-    
-    // $(".message-button").on("click", function(event){
-    //     event.preventDefault();
-    //     $(".message-modal").addClass("is-active");
-    //   // $(".modal-card-title").text("Connect with " + data[0].dogName);
-    // });
-    // $(".delete, .cancel-button").on("click", function (event) {
-    //     event.preventDefault();
-    //     $(".modal").removeClass("is-active");
-    //   });
+    $(".message-button").on("click", function(event){
+        event.preventDefault();
+        $(".message-modal").addClass("is-active");
+      // $(".modal-card-title").text("Connect with " + data[0].dogName);
+    });
+    $(".delete, .cancel-button").on("click", function (event) {
+        event.preventDefault();
+        $(".modal").removeClass("is-active");
+      });
   
-    //   $(".save-button, .back-button").on("click", function (event) {
-    //     event.preventDefault();
-    //     location.reload();
-    //   });
+      $(".save-button, .back-button").on("click", function (event) {
+        event.preventDefault();
+        location.reload();
+      });
 
-    //   $(".meet-button").on("click", function(event){
-    //     event.preventDefault();
-    //     $(".meet-modal").addClass("is-active");
+      $(".meet-button").on("click", function(event){
+        event.preventDefault();
+        $(".meet-modal").addClass("is-active");
         
-    // });
+    });
 });
